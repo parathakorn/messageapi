aa96ab0eda6e97f95b02257a10999e389f790b70
parathakorn <korn9996@gmail.com>
2020-09-01 10:41:16 +0700

    Add files via upload

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1654880290'); 				//กรอก ค่า Channel ID
+define('LINE_MESSAGE_CHANNEL_SECRET','11244a6442f44298bc083b311d05e82f'); 	//กรอกค่า Channel secret
+define('LINE_MESSAGE_ACCESS_TOKEN','8IFoohQjh502i+Ch+2+C8+73GuKwMneh/H+CYe+iENi2qdLnv3/wn4c8H3o7irFc0bFhaNuK7JQCC1Zq6vczjMqfR2nddoIVTVi9unF5j6a2pZFwv3bzwxeGAxRctptuANsiZJ1Dgd+ia5oWarsJugdB04t89/1O/w1cDnyilFU='); //กรอกค่า Channel access token
+?>
